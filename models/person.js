a49a5ae8ea33db902748922b21598332d550c621
Mohkula <matias.himanen@helsinki.fi>
2021-08-04 12:56:06 +0300

    tehtävät 3.20 ja 3.21

--- models/person.js
+++ models/person.js
@@ -14,8 +14,17 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
   })
 
   const PersonSchema = new mongoose.Schema({
-    name: {type: String, unique: true},
-    number: String,
+    name: 
+    {
+      type: String, 
+      unique: true,
+      minlength: 3
+    },
+    number: 
+    {
+      type: String,
+      minlength: 8
+    }
   })
   PersonSchema.plugin(uniqueValidator)
   
